116ef8f95c20f60fa315c9a7e1ab65c9e9d310b3
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-10-19 10:24:23 -0300

    Create um.js

---
+++ questoesFor/um.js
@@ -0,0 +1,5 @@
+/* Escreva um programa que conte de 1 a 10 e, para cada número, exiba o resultado da multiplicação desse número por 5. */
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`O número ${i} multiplicado por 5 é igual a ${i * 5}`);
+}
