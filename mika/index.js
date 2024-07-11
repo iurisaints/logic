73da17c0e804b80e1d16ee8d62985b895e506996
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2024-07-11 19:39:29 -0300

    Create index.js

---
+++ mika/index.js
@@ -0,0 +1,12 @@
+//prompt == input de dados
+//camelCase == tipo de escrita de variável
+let primeiro_nome = prompt("Insira seu primeiro nome, por favor: ")
+let meu_sobrenome = prompt("Insira seu sobrenome: ")
+
+//log e também o alert == output de dados
+alert(`Meu nome é ${primeiro_nome} e meu sobrenome é ${meu_sobrenome}`)
+
+
+//escrita de vairável no python == snake_case
+//kebab-case
+//quando fechar a aba pode escrever: localhost:5500
