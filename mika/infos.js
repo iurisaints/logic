4feae670612f76ad24e10f437fd52e4ba7e3a356
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2024-07-11 19:39:46 -0300

    Create infos.js

---
+++ mika/infos.js
@@ -0,0 +1,11 @@
+//let = variáveis que podem mudar de valor
+//number = variáveis numéricas
+let idade = 25
+
+//const = valor imutável
+//string = variaveis de texto
+const nome = 'iuri'
+
+//boolean é o tipo binário de valores de variável
+// o ! torna o valor inverso
+let teste = false
